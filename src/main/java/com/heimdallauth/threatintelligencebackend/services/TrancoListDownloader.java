bc411a31c7f1a5a4f77edbc31c74d29687bd576d
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-05-15 23:18:35 -0600

    Updates to download the tranco list on app startup

--- src/main/java/com/heimdallauth/threatintelligencebackend/services/TrancoListDownloader.java
+++ src/main/java/com/heimdallauth/threatintelligencebackend/services/TrancoListDownloader.java
@@ -7,20 +7,22 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 @Service
 @Slf4j
 public class TrancoListDownloader {
     private static final String TRANCO_LIST_ID = "V9WWN"; // Replace with latest list ID
     private static final String TRANCO_URL = "https://tranco-list.eu/download/" + TRANCO_LIST_ID + "/csv";
-    static final Path TRANCO_DEST = Paths.get("src/main/resources/tranco_top_1m.csv");
+    static final Path TRANCO_DEST = Paths.get(System.getProperty("user.dir"), "tranco_top_1m.csv");
 
     @PostConstruct
     public void downloadTrancoList() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(TRANCO_URL).openStream());
+        try (BufferedInputStream in = new BufferedInputStream(URI.create(TRANCO_URL).toURL().openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(TRANCO_DEST.toFile())) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
